4b6c2e7e4504f319bd7854f8ae00168bfbb42f59
利辺羅エルネスト <rivera_ernesto@cyberagent.co.jp>
2013-07-19 13:57:12 +0900

    Fix Demo project build

--- Demo/Pods/Local Podspecs/NBUKit.podspec
+++ Demo/Pods/Local Podspecs/NBUKit.podspec
@@ -3,7 +3,7 @@
 Pod::Spec.new do |s|
     
     s.name          = "NBUKit"
-    s.version       = "1.9.3"
+    s.version       = "1.9.5"
     s.platform      = :ios
     s.summary       = "Customizable image editing, filters, picker and other UIKit subclasses."
     s.homepage      = "http://cyberagent.github.io/iOS-NBUKit/"
--- Demo/Pods/Pods-environment.h
+++ Demo/Pods/Pods-environment.h
@@ -58,71 +58,71 @@
 #define COCOAPODS_POD_AVAILABLE_NBUKit
 #define COCOAPODS_VERSION_MAJOR_NBUKit 1
 #define COCOAPODS_VERSION_MINOR_NBUKit 9
-#define COCOAPODS_VERSION_PATCH_NBUKit 3
+#define COCOAPODS_VERSION_PATCH_NBUKit 5
 
 // NBUKit/Assets
 #define COCOAPODS_POD_AVAILABLE_NBUKit_Assets
 #define COCOAPODS_VERSION_MAJOR_NBUKit_Assets 1
 #define COCOAPODS_VERSION_MINOR_NBUKit_Assets 9
-#define COCOAPODS_VERSION_PATCH_NBUKit_Assets 3
+#define COCOAPODS_VERSION_PATCH_NBUKit_Assets 5
 
 // NBUKit/Image
 #define COCOAPODS_POD_AVAILABLE_NBUKit_Image
 #define COCOAPODS_VERSION_MAJOR_NBUKit_Image 1
 #define COCOAPODS_VERSION_MINOR_NBUKit_Image 9
-#define COCOAPODS_VERSION_PATCH_NBUKit_Image 3
+#define COCOAPODS_VERSION_PATCH_NBUKit_Image 5
 
 // NBUKit/Library
 #define COCOAPODS_POD_AVAILABLE_NBUKit_Library
 #define COCOAPODS_VERSION_MAJOR_NBUKit_Library 1
 #define COCOAPODS_VERSION_MINOR_NBUKit_Library 9
-#define COCOAPODS_VERSION_PATCH_NBUKit_Library 3
+#define COCOAPODS_VERSION_PATCH_NBUKit_Library 5
 
 // NBUKit/Library/MPNotificationView
 #define COCOAPODS_POD_AVAILABLE_NBUKit_Library_MPNotificationView
 #define COCOAPODS_VERSION_MAJOR_NBUKit_Library_MPNotificationView 1
 #define COCOAPODS_VERSION_MINOR_NBUKit_Library_MPNotificationView 9
-#define COCOAPODS_VERSION_PATCH_NBUKit_Library_MPNotificationView 3
+#define COCOAPODS_VERSION_PATCH_NBUKit_Library_MPNotificationView 5
 
 // NBUKit/Library/MPNotificationView/OBGradientView
 #define COCOAPODS_POD_AVAILABLE_NBUKit_Library_MPNotificationView_OBGradientView
 #define COCOAPODS_VERSION_MAJOR_NBUKit_Library_MPNotificationView_OBGradientView 1
 #define COCOAPODS_VERSION_MINOR_NBUKit_Library_MPNotificationView_OBGradientView 9
-#define COCOAPODS_VERSION_PATCH_NBUKit_Library_MPNotificationView_OBGradientView 3
+#define COCOAPODS_VERSION_PATCH_NBUKit_Library_MPNotificationView_OBGradientView 5
 
 // NBUKit/Library/NBUCompatibility
 #define COCOAPODS_POD_AVAILABLE_NBUKit_Library_NBUCompatibility
 #define COCOAPODS_VERSION_MAJOR_NBUKit_Library_NBUCompatibility 1
 #define COCOAPODS_VERSION_MINOR_NBUKit_Library_NBUCompatibility 9
-#define COCOAPODS_VERSION_PATCH_NBUKit_Library_NBUCompatibility 3
+#define COCOAPODS_VERSION_PATCH_NBUKit_Library_NBUCompatibility 5
 
 // NBUKit/Library/RBVolumeButtons
 #define COCOAPODS_POD_AVAILABLE_NBUKit_Library_RBVolumeButtons
 #define COCOAPODS_VERSION_MAJOR_NBUKit_Library_RBVolumeButtons 1
 #define COCOAPODS_VERSION_MINOR_NBUKit_Library_RBVolumeButtons 9
-#define COCOAPODS_VERSION_PATCH_NBUKit_Library_RBVolumeButtons 3
+#define COCOAPODS_VERSION_PATCH_NBUKit_Library_RBVolumeButtons 5
 
 // NBUKit/Library/RestKit Support
 #define COCOAPODS_POD_AVAILABLE_NBUKit_Library_RestKit_Support
 #define COCOAPODS_VERSION_MAJOR_NBUKit_Library_RestKit_Support 1
 #define COCOAPODS_VERSION_MINOR_NBUKit_Library_RestKit_Support 9
-#define COCOAPODS_VERSION_PATCH_NBUKit_Library_RestKit_Support 3
+#define COCOAPODS_VERSION_PATCH_NBUKit_Library_RestKit_Support 5
 
 // NBUKit/Picker
 #define COCOAPODS_POD_AVAILABLE_NBUKit_Picker
 #define COCOAPODS_VERSION_MAJOR_NBUKit_Picker 1
 #define COCOAPODS_VERSION_MINOR_NBUKit_Picker 9
-#define COCOAPODS_VERSION_PATCH_NBUKit_Picker 3
+#define COCOAPODS_VERSION_PATCH_NBUKit_Picker 5
 
 // NBUKit/Resources
 #define COCOAPODS_POD_AVAILABLE_NBUKit_Resources
 #define COCOAPODS_VERSION_MAJOR_NBUKit_Resources 1
 #define COCOAPODS_VERSION_MINOR_NBUKit_Resources 9
-#define COCOAPODS_VERSION_PATCH_NBUKit_Resources 3
+#define COCOAPODS_VERSION_PATCH_NBUKit_Resources 5
 
 // NBUKit/UI
 #define COCOAPODS_POD_AVAILABLE_NBUKit_UI
 #define COCOAPODS_VERSION_MAJOR_NBUKit_UI 1
 #define COCOAPODS_VERSION_MINOR_NBUKit_UI 9
-#define COCOAPODS_VERSION_PATCH_NBUKit_UI 3
+#define COCOAPODS_VERSION_PATCH_NBUKit_UI 5
 
