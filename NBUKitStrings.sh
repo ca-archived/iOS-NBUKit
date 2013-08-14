9cdc0481c132c7936f844782dd53a540261897e0
利辺羅エルネスト <rivera_ernesto@cyberagent.co.jp>
2013-08-14 17:26:13 +0900

    Add NBUKitStrings.sh to generate NBUKit.strings

---
+++ NBUKitStrings.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+#  NBUKitStrings.sh
+#  
+#
+#  Created by 利辺羅 on 2013/08/14.
+#
+
+cd Source
+find . -name "*.m" -print0 | xargs -0 genstrings -u -o ../NBUKitResources.bundle/en.lproj *.m
+mv ../NBUKitResources.bundle/en.lproj/Localizable.strings ../NBUKitResources.bundle/en.lproj/NBUKit.strings
