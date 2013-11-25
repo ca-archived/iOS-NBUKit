cc7258eb0d387f03e422e239689e5144886c83f8
利辺羅エルネスト <rivera_ernesto@cyberagent.co.jp>
2013-11-25 16:09:34 +0900

    Use if available NBULog or CocoaLumberjack, else fallback to NSLog

--- Source/NBUKitPrivate.h
+++ Source/NBUKitPrivate.h
@@ -20,6 +20,7 @@
 
 #import "NBUKit.h"
 
+
 // Localization
 #undef  NSLocalizedString
 #define NSLocalizedString(key, comment) \
@@ -29,7 +30,7 @@
 [NSBundle localizedStringForKey:key value:defaultValue table:@"NBUKit" backupBundle:NBUKit.resourcesBundle]
 
 
-// Use NBULog for logging when available
+// a) Use NBULog for logging when available
 #ifdef COCOAPODS_POD_AVAILABLE_NBULog
 
 #import "NBULog+NBUKit.h"
@@ -48,16 +49,43 @@
 #undef  NBULogVerbose
 #define NBULogVerbose(frmt, ...)LOG_OBJC_MAYBE(LOG_ASYNC_VERBOSE, NBUKIT_LOG_LEVEL, LOG_FLAG_VERBOSE, NBUKIT_LOG_CONTEXT, frmt, ##__VA_ARGS__)
 
+
+// b) Else try CocoaLumberjack
+#elif defined(COCOAPODS_POD_AVAILABLE_CocoaLumberjack)
+
+#ifdef DEBUG
+    #define NBUKitLogLevel LOG_LEVEL_VERBOSE
+#else
+    #define NBUKitLogLevel LOG_LEVEL_WARN
+#endif
+
+#define LOG_LEVEL_DEF   NBUKitLogLevel
+#import <CocoaLumberjack/DDLog.h>
+
+#define NBULogError(frmt, ...)      DDLogError(frmt, ##__VA_ARGS__)
+#define NBULogWarn(frmt, ...)       DDLogWarn(frmt, ##__VA_ARGS__)
+#define NBULogInfo(frmt, ...)       DDLogInfo(frmt, ##__VA_ARGS__)
+#define NBULogDebug(frmt, ...)      DDLogDebug(frmt, ##__VA_ARGS__)
+#define NBULogVerbose(frmt, ...)    DDLogVerbose(frmt, ##__VA_ARGS__)
+#define NBULogTrace()               NBULogDebug(@"%@", THIS_METHOD)
+
+
+// c) Else fallback to NSLog
 #else
 
-#define LOG_LEVEL 2
+#ifdef DEBUG
+    #define LOG_LEVEL 3
+#else
+    #define LOG_LEVEL 2
+#endif
 
-// NSLog macros
+#define THIS_METHOD                 NSStringFromSelector(_cmd)
 #define NBULogError(frmt, ...)      do{ if(LOG_LEVEL >= 1) NSLog((frmt), ##__VA_ARGS__); } while(0)
 #define NBULogWarn(frmt, ...)       do{ if(LOG_LEVEL >= 2) NSLog((frmt), ##__VA_ARGS__); } while(0)
 #define NBULogInfo(frmt, ...)       do{ if(LOG_LEVEL >= 3) NSLog((frmt), ##__VA_ARGS__); } while(0)
 #define NBULogDebug(frmt, ...)      do{ if(LOG_LEVEL >= 4) NSLog((frmt), ##__VA_ARGS__); } while(0)
 #define NBULogVerbose(frmt, ...)    do{ if(LOG_LEVEL >= 5) NSLog((frmt), ##__VA_ARGS__); } while(0)
+#define NBULogTrace()               NBULogDebug(@"%@", THIS_METHOD)
 
 #endif
 
