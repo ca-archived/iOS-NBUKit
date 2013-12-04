cb1b1784c3b7532965ae5a99fba7349d46d463cb
利辺羅エルネスト <rivera_ernesto@cyberagent.co.jp>
2013-12-04 18:01:44 +0900

    Update NBUKitStrings script

--- NBUKitStrings.sh
+++ NBUKitStrings.sh
@@ -7,5 +7,5 @@
 #
 
 cd Source
-find . -name "*.m" -print0 | xargs -0 genstrings -u -o ../NBUKitResources.bundle/en.lproj *.m
-mv ../NBUKitResources.bundle/en.lproj/Localizable.strings ../NBUKitResources.bundle/en.lproj/NBUKit.strings
+find . -name "*.m" -print0 | xargs -0 genstrings -u -o ../NBUResources.bundle/en.lproj *.m
+mv ../NBUResources.bundle/en.lproj/Localizable.strings ../NBUResources.bundle/en.lproj/NBUKit.strings
